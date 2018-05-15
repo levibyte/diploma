b016ad0bd64b606db810a738727a8da744ca0432
levibyte <kartol@gmx.com>
2018-05-15 12:54:15 +0400

    update compiler error

--- src/integ/test/Module.mk
+++ src/integ/test/Module.mk
@@ -1,6 +1,6 @@
-#LOCAL_INCS_FIXME:=-I include/integ
-#LOCAL_LIBS_FIXME:=-L $(LIB_DIR) -lLeviInterp
-#LOCAL_LIBS_FIXME:=-L $(LIB_DIR) $(LIB_DIR)/libLeviInterp.so
+LOCAL_INCS_FIXME:=-I include/ -I include/integ
+LOCAL_LIBS_FIXME:=-L $(LIB_DIR) -lLeviInterp
+LOCAL_LIBS_FIXME:=-L $(LIB_DIR) $(LIB_DIR)/libLeviInterp.so
 #MODULE:=integ
 LOCAL_LIBS_$(MODULE):=-L $(LIB_DIR) $(LIB_DIR)/libLeviInterp.so
 #LOCAL_LIBS_integ:=-L $(LIB_DIR) $(LIB_DIR)/libLeviInterp.so
